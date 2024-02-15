a8dee04e195bdf02912839f21406484013661583
kafleypl <155503438+kafleypl@users.noreply.github.com>
2024-02-14 22:06:58 -0500

    Update email.js

--- email.js
+++ email.js
@@ -1,12 +1,12 @@
-var shown = false;
+var emailShown = false;
 
-function showhideEmail() {
-  if (shown) {
-    document.getElementById('email').innerHTML = "Show my email";
-    shown = false;
-  } else {
-    var myemail = "<a href='mailto:kafleypl" + "@" + "ucmail.uc.edu'>kafleypl" + "@" + "ucmail.uc.edu</a>";
-    document.getElementById('email').innerHTML = myemail;
-    shown = true;
-  }
+function showHideEmail() {
+    var emailElement = document.getElementById('email');
+    if (emailShown) {
+        emailElement.innerHTML = 'kafleypl@mail.uc.edu';
+        emailShown = false;
+    } else {
+        emailElement.innerHTML = 'Show my email';
+        emailShown = true;
+    }
 }
